bd84f2405638147f12735eb9a301f38fe7761767
Majcon <majcon94@gmail.com>
2018-02-27 00:20:33 +0100

    2.7.0

--- includes/bot.lang.php
+++ includes/bot.lang.php
@@ -19,6 +19,9 @@
 	$lang['groupOnline_online'] = "[SIZE=10]{1} jest [COLOR=#00FF00][B]OnLine[/B][/COLOR] na kanale {2}[/SIZE]\n";
 	$lang['groupOnline_offline'] = "[SIZE=10]{1} jest [COLOR=#FF0000]Offline[/COLOR] od {2}[/SIZE]\n";
 
+	//newUser(); 
+	$lang['newUser_title'] = "[CENTER][SIZE=14][COLOR=#ff0000][B]Użytkownicy, którzy dołączyli wciągu 24h.[/B][/COLOR][/SIZE][/CENTER]\n\n{1}";
+
 	//poke();
 	$lang['error_admin_offline_poke'] = "Aktualnie nie ma administracji lub są zajęci";
 	$lang['success_admin_poke'] = "{1} potrzebuje Twojej pomocy";
@@ -33,24 +36,24 @@
 	$lang['kick_sprnick'] = "Prosimy o zmianę nicku. Dziękujemy.";
 
 	//statusTwitch()
-	$lang['online_statusTwitch'] = "[size=14][COLOR=#4b367c]Bazwa:[/color] [URL={1}][color=#00aa00]{2}[/color][/URL]\n[COLOR=#4b367c]Status:[/color] [color=#00aa00]Online[/color]\n[COLOR=#4b367c]Obecna gra:[/color] {3}\n[COLOR=#4b367c]Tytuł:[/color] {4}\n[COLOR=#4b367c]Oglądających:[/color] {5}\n[img]{6}[/img]";
-	$lang['offline_statusTwitch'] = "[size=14][COLOR=#4b367c]Bazwa:[/color] [URL=https://www.twitch.tv/{1}][color=#00aa00]{1}[/color][/URL]\n[COLOR=#4b367c]Status:[/color] [color=#ff0000]Offline[/color][/size]";
+	$lang['online_statusTwitch'] = "[size=14][COLOR=#4b367c]Nazwa:[/color] [URL={1}][color=#00aa00]{2}[/color][/URL]\n[COLOR=#4b367c]Status:[/color] [color=#00aa00]Online[/color]\n[COLOR=#4b367c]Obecna gra:[/color] {3}\n[COLOR=#4b367c]Tytuł:[/color] {4}\n[COLOR=#4b367c]Oglądających:[/color] {5}\n[img]{6}[/img]";
+	$lang['offline_statusTwitch'] = "[size=14][COLOR=#4b367c]Nazwa:[/color] [URL=https://www.twitch.tv/{1}][color=#00aa00]{1}[/color][/URL]\n[COLOR=#4b367c]Status:[/color] [color=#ff0000]Offline[/color][/size]";
 
 	//statusYt()
 	$lang['channel_description_statusYt'] = "[size=14][color=#ff0000]Nazwa:[/color] [URL=https://www.youtube.com/channel/{1}]{2}[/URL]\n[color=#ff0000]Subskrypcji:[/color] {3}\n[color=#ff0000]Wyświetlenia:[/color] {4}\n[color=#ff0000]Opis:[/color] {5}[/size]";
 	$lang['channel_name_statusYt'] = "{1} ({2})";
 
 	//wyswietl_czas();
-	$lang['time_d1_wyswietl_czas'] = "Dnień.";
-	$lang['time_d2_wyswietl_czas'] = "Dni.";
-	$lang['time_h1_wyswietl_czas'] = "Godzinę.";
-	$lang['time_h2_wyswietl_czas'] = "Godziny.";
-	$lang['time_h3_wyswietl_czas'] = "Godzin.";
-	$lang['time_i1_wyswietl_czas'] = "Minutę.";
-	$lang['time_i2_wyswietl_czas'] = "Minuty.";
-	$lang['time_i3_wyswietl_czas'] = "Minut.";
-	$lang['time_s1_wyswietl_czas'] = "Sekundę.";
-	$lang['time_s2_wyswietl_czas'] = "Sekundy.";
-	$lang['time_s3_wyswietl_czas'] = "Sekund.";
+	$lang['time_d1_wyswietl_czas'] = "dnień";
+	$lang['time_d2_wyswietl_czas'] = "dni";
+	$lang['time_h1_wyswietl_czas'] = "godzinę";
+	$lang['time_h2_wyswietl_czas'] = "godziny";
+	$lang['time_h3_wyswietl_czas'] = "godzin";
+	$lang['time_i1_wyswietl_czas'] = "minutę";
+	$lang['time_i2_wyswietl_czas'] = "minuty";
+	$lang['time_i3_wyswietl_czas'] = "minut";
+	$lang['time_s1_wyswietl_czas'] = "sekundę";
+	$lang['time_s2_wyswietl_czas'] = "sekundy";
+	$lang['time_s3_wyswietl_czas'] = "sekund";
 	
 ?>
